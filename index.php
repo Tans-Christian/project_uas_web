345d1fbc354a0ab2cb34366fefe745fb9e4eb480
Tans-Christian <oktan.christian@gmail.com>
2025-02-02 17:34:35 +0700

    Update V1

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 include 'config/koneksi.php';
+include 'includes/header.php';
 ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <title>Company Info</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-4">
@@ -14,17 +16,20 @@ include 'config/koneksi.php';
         <a href="register.php" class="btn btn-success">Register</a>
 
         <h3 class="mt-4">Informasi Perusahaan</h3>
-        <ul>
+        <div class="list-group">
             <?php
             $stmt = $conn->prepare("SELECT * FROM information");
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             foreach ($result as $row) {
-                echo "<li><a href='detail.php?id=" . $row['id'] . "'>" . htmlspecialchars($row['title']) . "</a></li>";
-            }
-            ?>
-        </ul>
+               echo "<a href='detail.php?id=" . $row['id'] . "' class='btn btn-primary btn-block mb-2'>" . htmlspecialchars($row['title']) . "</a>";
+             }
+             ?>
+        </div>
+
     </div>
 </body>
 </html>
+
+<?php include 'includes/footer.php';
